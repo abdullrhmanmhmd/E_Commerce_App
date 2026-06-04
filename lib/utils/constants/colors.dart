109a9764e45d51d5b72197a1c78e563695d0d83a
abdullrhman <abdullrhmanmhmd1@gmail.com>
2026-06-04 17:54:44 +0300

    just a test

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,6 @@
+class colors{
+  static const String primaryColor = "#FF5733";
+  static const String secondaryColor = "#33C1FF";
+  static const String backgroundColor = "#F0F0F0";
+  static const String textColor = "#333333";
+}
